2f91488febf6a0405f36d9acecb808d008212247
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2025-04-24 18:25:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,17 @@ const cors = require('cors');
 const paymentRoutes = require('./routes/payment.js');
 
 const app = express();
-app.use(cors());
+
+// Configure CORS to allow requests from your frontend URL
+const corsOptions = {
+  origin: 'https://qr-code-slqs.vercel.app', // Allow this origin
+  methods: ['GET', 'POST'], // Specify allowed methods
+  credentials: true, // Allow credentials if needed
+};
+
+app.use(cors(corsOptions)); // Use the CORS middleware with options
 app.use(express.json());
 
 app.use('/api', paymentRoutes);
 
-app.listen(5000, () => console.log("Server running at http://localhost:5000"));
+app.listen(5000, () => console.log("Server running at 5000"));
