4a09016f0b293b2c5f585f961734e3d627bc650d
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2025-04-26 13:39:38 +0530

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Payment from "./pages/payment";
 import QRScanner from "./pages/Qrscanner";
 import PdfPreviewer from "./pages/pdf-maker";
-
+import NotSupported from "./pages/not-supported";
 export default function App() {
   return (
     <Router>
@@ -11,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Payment />} />
         <Route path="/scan" element={<QRScanner />} />
         <Route path="/pdf" element={<PdfPreviewer />} />
+        <Route path="/not-supported" element={<NotSupported/>}/>
       </Routes>
     </Router>
   );
