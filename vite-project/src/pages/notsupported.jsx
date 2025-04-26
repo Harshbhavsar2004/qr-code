706f7de0eb27d2d6600427f1095cbb534e82f3b7
Harshal Bhavsar <154224487+Harshbhavsar2004@users.noreply.github.com>
2025-04-26 13:37:26 +0530

    Create notsupported.jsx

---
+++ vite-project/src/pages/notsupported.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotSupported() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Page Not Supported</h1>
+      <p>This page is only accessible on mobile devices. Please try accessing it from a mobile device.</p>
+    </div>
+  );
+} 
